052dc61106265c42fb4d3d6198c1102f64880a65
Inbar Barkai <inbarbarkai@gmail.com>
2021-10-02 12:04:18 +0300

    Implemented read tests.

--- tests/fatcat.Tests/FatSystemTests.cs
+++ tests/fatcat.Tests/FatSystemTests.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using fatcat.Core;
 using FluentAssertions;
@@ -69,6 +72,44 @@ namespace fatcat.Tests
 
             var entries = await system.GetEntries("/xyz");
             entries.Should().BeNull();
+        }
+
+        [InlineData("hello-world.img", "/hello.txt", "Hello world!\n")]
+        [InlineData("hello-world.img", "/files/other_file.txt", "Hello!\nThis is another file!\n")]
+        [Theory]
+        public async Task ReadFileTest(string imagePath, string filePath, string expectedContent)
+        {
+            using var serviceProvider = TestHelper.CreateDefault()
+              .BuildServiceProvider();
+            using var stream = TestHelper.GetTestFileStream(imagePath);
+
+            var system = serviceProvider.GetRequiredService<FatSystem>();
+            await system.Initialize(stream).ConfigureAwait(false);
+
+            using var contentStream = new MemoryStream();
+            await system.ReadFile(filePath, contentStream);
+
+            var actualContent = Encoding.UTF8.GetString(contentStream.ToArray()).Trim('\0');
+            actualContent.Should().Be(expectedContent);
+        }
+
+        [InlineData("hello-world.img", 3UL, 13UL, "Hello world!\n")]
+        [InlineData("hello-world.img", 5UL, 29UL, "Hello!\nThis is another file!\n")]
+        [Theory]
+        public async Task ReadFileClusterTest(string imagePath, ulong cluster, ulong size, string expectedContent)
+        {
+            using var serviceProvider = TestHelper.CreateDefault()
+              .BuildServiceProvider();
+            using var stream = TestHelper.GetTestFileStream(imagePath);
+
+            var system = serviceProvider.GetRequiredService<FatSystem>();
+            await system.Initialize(stream).ConfigureAwait(false);
+
+            using var contentStream = new MemoryStream();
+            await system.ReadFile(cluster, size, contentStream, false);
+
+            var actualContent = Encoding.UTF8.GetString(contentStream.ToArray()).Trim('\0');
+            actualContent.Should().Be(expectedContent);
         }
     }
 }
